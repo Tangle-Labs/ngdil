501b8e255a949f92aead8939b1fbad5fbf8f56ed
Coodos <info@coodos.co>
2023-04-15 21:55:38 +0530

    feat: typography

--- client/src/lib/components/ui/Button/Button.svelte
+++ client/src/lib/components/ui/Button/Button.svelte
@@ -1,62 +1,43 @@
 <style lang="scss">
 	.button {
-		background-color: var(--primary);
 		padding: 8px 16px;
-		color: var(--primary-alt);
 		border: 0;
 		transition: all 0.5s;
 		border-radius: 5px;
-		font-size: 1rem;
 		outline: none;
+		text-transform: capitalize;
+		box-shadow: var(--box-shadow);
 
 		&:hover {
 			cursor: pointer;
-			background-color: var(--primary-hover);
 		}
-	}
 
-	.alternative {
-		background-color: var(--secondary);
-		color: var(--secondary-alt);
+		&.primary {
+			background: var(--primary-900);
 
-		&:hover {
-			background-color: var(--secondary-hover);
+			&:hover {
+				background: var(--primary-700);
+			}
 		}
-	}
 
-	.small {
-		font-size: 0.8rem;
-		padding: 6px 12px;
-	}
+		&.secondary {
+			background: var(--secondary-900);
 
-	.circular {
-		padding: 4px;
-		box-sizing: border-box;
-		font-size: 0.8rem;
-		width: 25px;
-		height: 25px;
-		border-radius: 20px;
-		text-transform: uppercase;
-	}
-
-	.large {
-		font-size: 1.2rem;
-		padding: 10px 20px;
+			&:hover {
+				background: var(--secondary-700);
+			}
+		}
 	}
 </style>
 
 <script lang="ts">
-	export let variant: "primary" | "secondary" = "primary";
+	export let variant: "primary" | "secondary" | "white" = "primary";
+	import Typography from "../Typography/Typography.svelte";
 	export let label: string;
 	export let onClick: (...args: any[]) => void;
-	export let size: "large" | "medium" | "small" | "circular" = "medium";
 </script>
 
-<button
-	class="button"
-	class:alternative="{variant === 'secondary'}"
-	class:large="{size === 'large'}"
-	class:small="{size === 'small'}"
-	class:circular="{size === 'circular'}"
-	on:click="{onClick}">{label}</button
->
+<button class="{`button ${variant}`}" on:click="{onClick}">
+	<Typography variant="button" color="{variant === 'primary' ? '--white-300' : ''}"
+		>{label}</Typography>
+</button>
---
+++ client/src/lib/components/ui/Typography/Typography.svelte
@@ -0,0 +1,62 @@
+<style lang="scss">
+	.main-font {
+		font-family: var(--header-font);
+	}
+
+	.kw1c-font {
+		font-family: var(--kw1c-font);
+	}
+
+	.typography {
+		color: var(--black-900);
+	}
+
+	.heading {
+		font-size: var(--header-text-size);
+		font-weight: 500;
+	}
+
+	.card-header {
+		font-size: var(--card-header-size);
+	}
+
+	.status {
+		font-size: var(--status-text-size);
+		color: var(--green-900);
+		font-weight: 600;
+	}
+
+	.button {
+		font-size: var(--button-text-size);
+		font-weight: 500;
+	}
+
+	.list {
+		font-size: var(--list-text-size);
+		color: var(--black-700);
+		font-weight: 500;
+	}
+
+	.body {
+		font-size: var(--body-text-size);
+		color: var(--black-700);
+		font-weight: 300;
+	}
+
+	.sub-text {
+		font-size: var(--sub-text-size);
+		color: var(--black-500);
+		font-weight: 300;
+	}
+</style>
+
+<script lang="ts">
+	import type { TypographyVariants } from "./Typography.types";
+	export let variant: TypographyVariants = "body";
+	export let fontVariant: "main" | "kw1c" = "main";
+	export let color = "";
+</script>
+
+<span
+	class="{`typography ${variant} ${fontVariant}-font`}"
+	style="{color && `color: var(${color});`}"><slot /></span>
