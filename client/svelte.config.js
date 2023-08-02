22fca19d2cfc2978c158e155ea0d3c78b148b31e
Coodos <info@coodos.co>
2023-08-03 00:17:24 +0530

    fix: build

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,9 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: "app.html"
+		}),
 		env: {
 			dir: "../"
 		}
