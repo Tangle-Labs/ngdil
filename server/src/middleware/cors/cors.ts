d48752c9d4739e467fb40080f9f5bbdac3c25411
Coodos <info@coodos.co>
2023-08-03 00:28:53 +0530

    feat: cors config

--- server/src/middleware/cors/cors.ts
+++ server/src/middleware/cors/cors.ts
@@ -1,7 +1,5 @@
 export const corsConfig = {
-	origin: function (origin: any, callback: any) {
-		callback(null, true);
-	},
+	origin: "ngdil-demo.tanglelabs.io",
 	credentials: true,
 	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 	allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "token", "Authorization"]
