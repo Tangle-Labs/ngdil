c07172261cda715459868ffd21d9a1f9a344a5ba
Coodos <info@coodos.co>
2023-08-03 10:22:56 +0530

    allow cors

--- server/src/middleware/cors/cors.ts
+++ server/src/middleware/cors/cors.ts
@@ -1,18 +1,8 @@
 import { CorsOptions } from "cors";
 
-var whitelist = [
-	"http://localhost:5169",
-	"http://localhost",
-	"https://ngdil-demo.tanglelabs.io",
-	"http://ngdil-demo.tanglelabs.io"
-];
 export const corsConfig: CorsOptions = {
 	origin: function (origin, callback) {
-		if (whitelist.indexOf(origin) !== -1) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
+		callback(null, true);
 	},
 	// origin: "http://localhost:5169",
 
