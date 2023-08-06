9dbeaef650973af54e6a61ce72ae00dcc053e5c4
Merul Dhiman <69296233+coodos@users.noreply.github.com>
2023-08-07 05:28:36 +0530

    Fix/same site cookie specifier (#14)
    * fix: add missing same site prop which was defaulting to lax
    
    * fix: add missing secure header

--- server/src/middleware/auth/auth.middleware.ts
+++ server/src/middleware/auth/auth.middleware.ts
@@ -17,7 +17,8 @@ export const userDeserializer = asyncHandler(
 			res.cookie("refreshToken", token, {
 				maxAge: 365 * 24 * 60 * 60 * 1000,
 				httpOnly: true,
-				sameSite: "none"
+				sameSite: "none",
+				secure: true
 			});
 		}
 
