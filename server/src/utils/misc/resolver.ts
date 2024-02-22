7cf9767217c35ebde156cf0c885270ae86fbe5ec
Coodos <info@coodos.co>
2024-02-22 12:32:23 +0530

    feat: add did web resolver

--- server/src/utils/misc/resolver.ts
+++ server/src/utils/misc/resolver.ts
@@ -1,5 +1,7 @@
 import { Resolver } from "did-resolver";
 import KeyResolver from "key-did-resolver";
+import * as WebResolver from "web-did-resolver";
 
 const keyDIDResolver = KeyResolver.getResolver();
-export const resolver = new Resolver(keyDIDResolver);
+const webDidResolver = WebResolver.getResolver();
+export const resolver = new Resolver({ ...keyDIDResolver, ...webDidResolver });
