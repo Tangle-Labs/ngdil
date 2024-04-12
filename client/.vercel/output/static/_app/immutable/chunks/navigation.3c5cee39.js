285c0e7c2dc27c63afa735aa2c87df0960695e14
Coodos <info@coodos.co>
2024-04-12 23:05:27 +0530

    feat: adapter vercel

---
+++ client/.vercel/output/static/_app/immutable/chunks/navigation.3c5cee39.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.0b1e745c.js";const e=o("goto");export{e as g};
---
+++ client/.vercel/output/static/_app/immutable/chunks/onboarding.store.0b9accba.js
@@ -0,0 +1,1 @@
+import{p as r}from"./index.80fd66bc.js";const p=r("onboardingCurrStep",0);export{p as c};
