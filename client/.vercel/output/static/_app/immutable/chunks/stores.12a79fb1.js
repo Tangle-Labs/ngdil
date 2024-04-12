3de73d29ccd5ffbc82405c87abf8ef9902ff586f
Merul Dhiman <69296233+coodos@users.noreply.github.com>
2024-04-12 23:08:09 +0530

    Fix/deployments (#37)
    * feat: adapter auto
    
    * feat: dapter
    
    * feat: adapter vercel

---
+++ client/.vercel/output/static/_app/immutable/chunks/stores.12a79fb1.js
@@ -0,0 +1,1 @@
+import"./index.c696c2c0.js";import{d as e}from"./singletons.0b1e745c.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
