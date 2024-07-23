bc518b25af66fc9d5763b58034ece883fcb1db50
Coodos <info@coodos.co>
2024-07-23 11:05:43 +0530

    chore: add jwk resolver

--- server/src/utils/misc/resolver.ts
+++ server/src/utils/misc/resolver.ts
@@ -1,7 +1,12 @@
 import { Resolver } from "did-resolver";
 import KeyResolver from "key-did-resolver";
 import * as WebResolver from "web-did-resolver";
+import JwkResolver from "@sphereon/did-resolver-jwk";
 
 const keyDIDResolver = KeyResolver.getResolver();
 const webDidResolver = WebResolver.getResolver();
-export const resolver = new Resolver({ ...keyDIDResolver, ...webDidResolver });
+export const resolver = new Resolver({
+	...keyDIDResolver,
+	...webDidResolver,
+	...JwkResolver.getDidJwkResolver()
+});
