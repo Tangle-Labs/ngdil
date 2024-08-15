f68bb76802e57283082b16b1262c9fc572820ea8
Coodos <info@coodos.co>
2024-08-15 19:40:02 +0530

    chore: add ebsi did key resolver

--- server/src/utils/misc/resolver.ts
+++ server/src/utils/misc/resolver.ts
@@ -2,22 +2,12 @@ import { DIDResolutionOptions, Resolvable, Resolver, ParsedDID } from "did-resol
 import KeyResolver from "key-did-resolver";
 import * as WebResolver from "web-did-resolver";
 import JwkResolver from "@sphereon/did-resolver-jwk";
+import DidKeyResolver from "@sphereon/did-resolver-key";
 
-const keyDIDResolver = KeyResolver.getResolver();
+const keyResolver = DidKeyResolver.getResolver();
 const webDidResolver = WebResolver.getResolver();
-const { key } = keyDIDResolver;
-console.log(key);
 export const resolver = new Resolver({
-	key: async (
-		did: string,
-		parsed: ParsedDID,
-		resolver: Resolvable,
-		options: DIDResolutionOptions
-	) => {
-		const doc = await key(did, parsed, resolver, options);
-		console.log("resolved", JSON.stringify(doc));
-		return doc;
-	},
 	...webDidResolver,
-	...JwkResolver.getDidJwkResolver()
+	...JwkResolver.getDidJwkResolver(),
+	...keyResolver
 });
